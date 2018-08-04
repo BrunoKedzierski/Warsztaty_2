2a2f513ecb9e9cff412d3d03f4a02f48b23bf8db
bruno <bruno.kedzierski@wp.pl>
2018-08-04 13:40:18 +0200

    Group abd Users classes

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -1,11 +1,20 @@
 package pl.coderslab;
 
 
-import java.sql.Connection;
+import pl.coderslab.Model.User;
+import service.DbService;
+
+import java.sql.SQLException;
+
 
 public class Main {
-
     public static void main(String[] args) {
 
+User.getUserById(23);
+
+
+
+
     }
+
 }
